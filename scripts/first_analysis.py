5623df2c142c1331cfa545bbbcd4560b1f2ca2b5
Francesca Schiavone <schiavone.francesca@outlook.it>
2025-09-13 17:02:37 +0200

    first story

--- scripts/first_analysis.py
+++ scripts/first_analysis.py
@@ -1,7 +1,7 @@
 import spacy
 import pandas as pd
 
-with open ("data\Il contadino astrologo.txt", "r", encoding="utf-8") as infile: 
+with open ("data\\Il contadino astrologo.txt", "r", encoding="utf-8") as infile: 
     text = infile.read()
 
 nlp = spacy.load("it_core_news_lg") 
@@ -16,21 +16,19 @@ ent_types = []
 sent_ids = []
 token_positions = []
 
-# Itera su tutti i token
 for sent_id, sent in enumerate(doc.sents):
     for token_pos, token in enumerate(sent):
         if not token.is_punct and not token.is_space:
             tokens.append(token.text)
             lemmas.append(token.lemma_)
             pos_tags.append(token.pos_)
-            morphs.append(token.morph.to_dict())  # restituisce un dict con caratteristiche morfologiche
+            morphs.append(token.morph.to_dict())  
             dep_rels.append(token.dep_)
-            heads.append(token.head.text)  # testa del token nella dipendenza
-            ent_types.append(token.ent_type_ if token.ent_type_ else "O")  # O = fuori da entità nominata
+            heads.append(token.head.text)  
+            ent_types.append(token.ent_type_ if token.ent_type_ else "O")  
             sent_ids.append(sent_id)
             token_positions.append(token_pos)
 
-# Crea dataframe pandas
 df = pd.DataFrame({
     "sentence_id": sent_ids,
     "token_position": token_positions,
@@ -44,18 +42,6 @@ df = pd.DataFrame({
 })
 
 df.head()
-# Salva in Excel o CSV
-df.to_excel("extracted_features.xlsx", index=False)
-# df.to_csv("extracted_features.csv", index=False)
 
-# print("Estrazione completata, file salvato.")
-# tokens = [token.text for token in doc if not token.is_punct and not token.is_space] #eliminazione punteggiatura e gli spazi (non tra parole ma tra frasi)
-
-# PoS = [token.pos_ for token in doc if not token.is_punct and not token.is_space]
-
-
-# df_pt1 = pd.DataFrame({"tokens": tokens, "PoS": PoS})
-
-# df_pt1.head()
-
-# df_pt1.to_excel("C:\\Users\\schia\\Desktop\\IMT\Alice\\Experiments\\df_pt1.xlsx",  index=False)
+df.to_csv("extracted_features1.csv", index=False)
+print("Estrazione completata, file salvato.")
